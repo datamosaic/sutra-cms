c04b7454690380383850efba303583862e9c5cda
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2011-02-20 20:36:02 +0000

    ordering on editable

--- trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_theme_1L_editable.js
+++ trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_theme_1L_editable.js
@@ -56,3 +56,91 @@ controller.deleteRecord()
 function REC_new(event) {
 	forms.WEB_0F_theme.ACTION_new_editable()
 }
+
+/**
+ *
+ * @properties={typeid:24,uuid:"414400F7-ED90-49B2-9616-BAD31B41EFEC"}
+ */
+function DIR_down()
+{
+	/*
+	 *	TITLE:		DIR_down
+	 *
+	 *	MODULE:		fw_NAV_navigation_standard
+	 *
+	 *	ABOUT:		Move navigation_item down in list
+	 *
+	 *	MODIFIED:	Aug 27, 2007 - Troy Elliott, Data Mosaic
+	 *
+	 */
+
+	 //if max index, exit
+	 if (foundset.getSelectedIndex() == foundset.getSize()) {
+		 return
+	 }
+	 
+	 foundset.sort('row_order asc')
+
+	 //if index = 1, set flag to avoid glitch recSelected
+	 //TODO: find issue
+	 if (foundset.getSelectedIndex() == 1) {
+		 var recOne = true
+	 }
+	 else {
+		 var recOne = false
+	 }
+
+	 //get current record
+	 var recordCurr = foundset.getRecord(foundset.getSelectedIndex())
+
+	 //get next record
+	 var recordNext = foundset.getRecord(foundset.getSelectedIndex() + 1)
+
+	 //swap with next record
+	 recordCurr.row_order = recordNext.row_order
+	 recordNext.row_order --
+
+	 foundset.sort('row_order asc') //need to order by id_navigation_item and category first?
+
+	 //TODO: find issue
+	 if (recOne) {
+		 controller.setSelectedIndex(2)
+	 }
+}
+
+/**
+ *
+ * @properties={typeid:24,uuid:"62AC953B-1A76-4958-BB92-4DE470F621F4"}
+ */
+function DIR_up()
+{
+	/*
+	 *	TITLE:		DIR_up
+	 *
+	 *	MODULE:		fw_NAV_navigation_standard
+	 *
+	 *	ABOUT:		Move navigation_item up in list
+	 *
+	 *	MODIFIED:	Aug 27, 2007 - Troy Elliott, Data Mosaic
+	 *
+	 */
+
+	 //if index = 1, exit
+	 if (foundset.getSelectedIndex() == 1) {
+		 return
+	 }
+
+	 foundset.sort('row_order asc')
+	 
+	 //get current record
+	 var recordCurr = foundset.getRecord(foundset.getSelectedIndex())
+
+	 //get previous record
+	 var recordPrev = foundset.getRecord(foundset.getSelectedIndex() - 1)
+
+	 //swap with previous record
+	 recordCurr.row_order = recordPrev.row_order
+	 recordPrev.row_order ++
+
+	 foundset.sort('row_order asc')
+}
