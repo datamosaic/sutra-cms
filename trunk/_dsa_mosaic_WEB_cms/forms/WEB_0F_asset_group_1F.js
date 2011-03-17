be0e41b61f461608c3d0e2bcb7248027b713540c
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2011-03-17 21:58:34 +0000

    pull asset actions from registered asset_type form

--- trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_asset_group_1F.js
+++ trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_asset_group_1F.js
@@ -4,3 +4,25 @@
 var _license_dsa_mosaic_WEB_cms = 'Module: _dsa_mosaic_WEB_cms \
 									Copyright (C) 2011 Data Mosaic \
 									MIT Licensed';
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @properties={typeid:24,uuid:"1F6CF598-4838-4641-86AC-D810EEC6718E"}
+ */
+function ACTIONS_list(event) {
+	//hand off to asset's actions
+	if (utils.hasRecords(web_asset_group_to_asset_type) && web_asset_group_to_asset_type.form_name &&
+		solutionModel.getForm(web_asset_group_to_asset_type.form_name).getFormMethod('ASSET_actions')) {
+		
+		forms[web_asset_group_to_asset_type.form_name].ASSET_actions(event)
+	}
+	else {
+		plugins.dialogs.showInformationDialog(
+					'Alert',
+					'This type of asset has no actions defined'
+				)
+	}
+}
