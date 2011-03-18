40dc673c47843e59682e7fd6dd30851b5f1eedbc
Kabootit <kabootit@users.noreply.github.com>
2011-03-18 11:26:01 +0000

    scale image asset from asset screen

--- _dsa_mosaic_WEB_cms_blocks/forms/WEB_0F_asset__image__P_scale.js
+++ _dsa_mosaic_WEB_cms_blocks/forms/WEB_0F_asset__image__P_scale.js
@@ -1,4 +1,19 @@
 /**
+ * @properties={typeid:35,uuid:"4EFB8CEF-D85F-4F71-A3DD-AFD1EC625FFF",variableType:-4}
+ */
+var _asset = null;
+
+/**
+ * @properties={typeid:35,uuid:"5F128A57-DBA4-4A5D-9085-882513C4BCD6",variableType:-4}
+ */
+var _metaHeight = null;
+
+/**
+ * @properties={typeid:35,uuid:"B3A3E14C-6C9A-4F19-AFB9-BCD9C9E0BF0F",variableType:-4}
+ */
+var _metaWidth = null;
+
+/**
  * @properties={typeid:35,uuid:"D4E60991-9F57-42B2-8E66-2B63EA7EEF04",variableType:4}
  */
 var _image_height;
@@ -27,44 +42,27 @@ var _image_width_original;
  *
  * @properties={typeid:24,uuid:"ACFDBD46-1CA4-4144-95F4-CF79A315AB37"}
  */
-function ACTION_cancel()
-{
-
-//not already ok to close, cancel
-if (!globals.CODE_hide_form) {
-	
-	//enaable closing the form
-	globals.CODE_hide_form = 1
-	
-	application.closeFormDialog('CMS_imageScale')
-}
+function ACTION_cancel() {
+	//not already ok to close, cancel
+	if (!globals.CODE_hide_form) {
+		//rollback and turn autosave on
+		databaseManager.rollbackEditedRecords()
+		databaseManager.setAutoSave(true)
+		
+		//enaable closing the form
+		globals.CODE_hide_form = 1
+		
+		application.closeFormDialog('CMS_imageScale')
+	}
 }
 
 /**
- *
  * @properties={typeid:24,uuid:"EB14E732-B22C-46F8-B488-C62667F892A7"}
  */
-function ACTION_ok()
-{
-	
-	// write new height and width back to data points
-
-// grab data record
-	var data = forms.WEB_0F_page__design.web_page_to_block_data_by_area_by_block
-	
-	// fill data records from FiD values
-	for (var i = 0; i < data.getSize(); i++) {
-		var record = data.getRecord(i + 1)
-		switch (record.data_key) {
-				case "width":
-					record.data_value = _image_width
-					break;
-				case "height":
-					record.data_value = _image_height
-				default:
-			break;
-		}
-	}
+function ACTION_ok() {
+	//data save ok
+	databaseManager.saveData()
+	databaseManager.setAutoSave(true)
 
 	//enable closing the form
 	globals.CODE_hide_form = 1
@@ -78,78 +76,54 @@ function ACTION_ok()
  *
  * @properties={typeid:24,uuid:"76E42D60-8B7C-4C8A-BF89-0BFC652E03C5"}
  */
-function ACTION_reset()
-{
+function ACTION_reset() {
 	// reset to original image sizes
-	_image_height = _image_height_original
-	_image_width = _image_width_original
+	_image_height = 
+	_metaHeight.data_value =
+		_image_height_original
+	
+	_image_width = 
+	_metaWidth.data_value =
+		_image_width_original
 }
 
 /**
  *
  * @properties={typeid:24,uuid:"7302C8F5-4ADA-47F8-B6FA-DC5669422414"}
  */
-function FLD_change_height()
-{
-	databaseManager.saveData()
-	_image_width = (_image_height * _image_width_original)/_image_height_original
-	databaseManager.saveData()
-	application.updateUI()
+function FLD_change_height() {
+	_metaWidth.data_value = (_image_height * _image_width_original)/_image_height_original
+}
+
+/**
+ * @properties={typeid:24,uuid:"337A797D-0A8B-4948-A897-19A82E12384E"}
+ */
+function FLD_change_name(oldValue, newValue, event) {
+	_asset.asset_title = _image_name
 }
 
 /**
  *
  * @properties={typeid:24,uuid:"89BD1453-04D7-4224-8FBB-31659B9C35B1"}
  */
-function FLD_change_width()
-{
-	databaseManager.saveData()
-	_image_height = (_image_width * _image_height_original)/_image_width_original
-	databaseManager.saveData()
-	application.updateUI()
+function FLD_change_width() {
+	_metaHeight.data_value = (_image_width * _image_height_original) / _image_width_original
 }
 
 /**
  *
  * @properties={typeid:24,uuid:"CA804DFE-3B86-4C73-A529-5551D59A477F"}
  */
-function FORM_on_load()
-{
+function FORM_on_load() {
 	// set the tab order sequence programatically
-	controller.setTabSequence(null);
+	controller.setTabSequence(null)
 }
 
 /**
  *
  * @properties={typeid:24,uuid:"CD3098A8-06A0-4DF0-AEF5-7B105FEA967E"}
  */
-function FORM_on_show()
-{
+function FORM_on_show() {
 	//disable closing the form
-	globals.CODE_hide_form = 0
-	
-	// grab data record
-	var data = forms.WEB_0F_page__design.web_page_to_block_data_by_area_by_block
-	
-	// fill FiD values from data records
-	for (var i = 0; i < data.getSize(); i++) {
-		var record = data.getRecord(i + 1)
-		switch (record.data_key) {
-				case "image_name":
-					_image_name = record.data_value
-					break;
-				case "width":
-					_image_width = record.data_value
-					break;
-				case "width_original":
-					_image_width_original = record.data_value
-					break;
-				case "height":
-					_image_height = record.data_value
-				case "height_original":
-					_image_height_original = record.data_value
-				default:
-			break;
-		}
-	}	
+	globals.CODE_hide_form = 0	
 }
