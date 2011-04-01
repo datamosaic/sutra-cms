043494d503f26ef8eb38078d0076638f0e19f39e
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2011-04-01 23:15:06 +0000

    generic cms page create

--- trunk/_dsa_mosaic_WEB_cms/globals.js
+++ trunk/_dsa_mosaic_WEB_cms/globals.js
@@ -601,6 +601,217 @@ function WEB_page_tree_to_popup(method,elem) {
 }
 
 /**
+ * @param	{String}	pageName Name for new page.
+ * @param	{Number}	[pageType=1] Type of page to create (see WEB_page_types valuelist).
+ * @param	{Number}	[parentID] Parent page primary key.
+ * @param	{Number}	[themeID] Theme primary key for page.
+ * @param	{Number}	[layoutID] Layout primary key for page.
+ * 
+ * @returns	{JSRecord}	Newly created page record.
+ * 
+ * @properties={typeid:24,uuid:"A7F7C059-3707-498F-A0B6-C1334EDA6A10"}
+ */
+function WEB_page_new(pageName,pageType,parentID,themeID,layoutID) {
+	if (!pageName) {
+		pageName = 'Untitled page'
+	}
+	
+	if (!pageType) {
+		pageType = 0
+	}
+	
+	//check if can add record
+	if (!globals.TRIGGER_registered_action_authenticate('cms page add')) {
+		plugins.dialogs.showErrorDialog(
+						'Error',
+						'You are not authorized to add new pages'
+				)
+		return
+	}
+	
+	//there are sites
+	if (utils.hasRecords(forms.WEB_0F_site.foundset)) {
+		//get the parent record
+		if (parentID) {
+			//find parent rec
+			var fsPage = databaseManager.getFoundSet('sutra_cms','web_page')
+			fsPage.find()
+			fsPage.id_page = parentID
+			var results = fsPage.search()
+			
+			if (results) {
+				var parentRec = fsPage.getRecord(1)
+			}
+		}
+		
+		var newRecord = fsPage.getRecord(fsPage.newRecord(false,true))
+		
+		newRecord.page_name = pageName
+		newRecord.page_type = pageType
+		newRecord.parent_id_page = (parentRec) ? parentRec.id_page : 0
+		newRecord.order_by = (parentRec) ? parentRec.web_page_to_page__child.getSize() + 1 : 1
+		newRecord.id_site = (parentRec) ? parentRec.id_site : forms.WEB_0F_site.id_site
+		
+		//create one version
+		var oneVersion = newRecord.web_page_to_version.getRecord(newRecord.web_page_to_version.newRecord(false,true))
+		oneVersion.version_number = 1
+		
+		//update valuelists
+		forms.WEB_0F_page__design.REC_on_select()
+		
+		databaseManager.saveData()
+		
+		//fill in defaults
+		var fsTheme = databaseManager.getFoundSet('sutra_cms','web_theme')
+		fsTheme.find()
+		fsTheme.theme_name = "Linds First Cut"
+		var results = fsTheme.search()
+		
+		if (results) {
+			//set theme
+			newRecord.id_theme = fsTheme.id_theme
+			
+			var fsLayout = databaseManager.getFoundSet('sutra_cms','web_layout')
+			fsLayout.find()
+			fsLayout.layout_name = "Product Controller"
+			fsLayout.id_theme = newRecord.id_theme
+			var results = fsLayout.search()
+			
+			//set layout
+			if (results) {
+				newRecord.id_theme_layout = fsLayout.id_layout
+			}
+			
+			databaseManager.saveData()
+			
+			//group to create as
+			//TODO: if they've sorted, this will not be the everybody/visitor group
+			var recGroup = newRecord.web_page_to_site.web_site_to_group.getRecord(1)
+			
+			var fsArea = databaseManager.getFoundSet('sutra_cms','web_area')
+			
+			// get editable regions based on layout selected
+			if (!utils.hasRecords(newRecord,'web_page_to_layout.web_layout_to_editable')) {
+				return 'No editables for selected layout'
+			}
+			
+			var fsRegions = newRecord.web_page_to_layout.web_layout_to_editable
+			
+			// create a page area record for each editable
+			if (fsRegions.getSize()) {
+				//sort
+				fsRegions.sort('row_order asc')
+				
+				//still manually set the order in case web_editable is out of sync (kind of likely)
+				var order = 1
+				for (var i = 1; i <= fsRegions.getSize(); i++) {
+					var tempEditableRec = fsRegions.getRecord(i)
+					
+					var areaRec = fsArea.getRecord(fsArea.newRecord(false, true))
+					
+					areaRec.area_name = tempEditableRec.editable_name
+					areaRec.id_editable = tempEditableRec.id_editable
+					areaRec.row_order = order ++ 
+					areaRec.id_group = recGroup.id_group
+					areaRec.id_version = oneVersion.id_version		
+					
+					//create a block record for each editable default
+					for (var j = 1; j <= tempEditableRec.web_editable_to_editable_default.getSize(); j++ ) {
+						var tempEditableDefaultRec = tempEditableRec.web_editable_to_editable_default.getRecord(j)
+						
+						var blockRec = areaRec.web_area_to_block.getRecord(areaRec.web_area_to_block.newRecord(false, true))
+						
+						blockRec.id_block_display = tempEditableDefaultRec.id_block_display
+						blockRec.id_block_type = tempEditableDefaultRec.id_block_type
+						blockRec.params = tempEditableDefaultRec.params
+						blockRec.id_scrapbook = tempEditableDefaultRec.id_scrapbook
+						blockRec.row_order = tempEditableDefaultRec.row_order
+						
+						// INPUT
+						//create a block_data record for each block_input
+						if ( tempEditableDefaultRec.web_editable_default_to_block_input ) {
+							for (var k = 1; k <= tempEditableDefaultRec.web_editable_default_to_block_input.getSize(); k++) {
+								var tempEditableDefaultDetailRec = tempEditableDefaultRec.web_editable_default_to_block_input.getRecord(k)
+		
+								var blockDataRec = blockRec.web_block_to_block_data.getRecord(blockRec.web_block_to_block_data.newRecord(false,true))
+								blockDataRec.data_key = tempEditableDefaultDetailRec.column_name
+							}
+						}
+						
+	//					// CONFIG
+	//					//create a ?? record for each block_configure
+	//					if ( tempEditableDefaultRec.web_editable_default_to_block_input ) {
+	//						for (var k = 1; k <= tempEditableDefaultRec.web_editable_default_to_block_input.getSize(); k++) {
+	//							var tempEditableDefaultDetailRec = tempEditableDefaultRec.web_editable_default_to_block_input.getRecord(k)
+	//	
+	//							var blockDataRec = blockRec.web_block_to_block_data.getRecord(blockRec.web_block_to_block_data.newRecord(false,true))
+	//							blockDataRec.data_key = tempEditableDefaultDetailRec.column_name
+	//						}
+	//					}
+	//					
+	//					// RESPONSE
+	//					//create a block_data_response record for each block_response
+	//					if ( tempEditableDefaultRec.web_editable_default_to_block_input ) {
+	//						for (var k = 1; k <= tempEditableDefaultRec.web_editable_default_to_block_input.getSize(); k++) {
+	//							var tempEditableDefaultDetailRec = tempEditableDefaultRec.web_editable_default_to_block_input.getRecord(k)
+	//	
+	//							var blockDataRec = blockRec.web_block_to_block_data.getRecord(blockRec.web_block_to_block_data.newRecord(false,true))
+	//							blockDataRec.data_key = tempEditableDefaultDetailRec.column_name
+	//						}
+	//					}
+					}
+				}
+			}
+			
+			//thrown in so that group data is properly filled...technically shouldn't save data until we're sure this won't be cancelled
+			databaseManager.saveData()
+			
+			// finish up
+			//fsArea.sort( "row_order asc" )
+			fsArea.setSelectedIndex(1)
+		}
+	
+		var fsPath = databaseManager.getFoundSet('sutra_cms','web_path')
+		var siteID = newRecord.id_site
+		
+		//add in path for this page
+		var pathNameWanted = newRecord.page_name
+		pathNameWanted = pathNameWanted.toLowerCase()
+		pathNameWanted = utils.stringReplace(pathNameWanted, ' ', '-')
+		
+		var pathName = pathNameWanted
+		var cnt = 1
+		
+		//we need to get into the loop
+		results = null
+		
+		while (results != 0) {
+			fsPath.find()
+			fsPath.id_site = siteID
+			fsPath.path = pathName
+			var results = fsPath.search()
+			
+			if (results) {
+				pathName = pathNameWanted + cnt++
+			}
+		}
+		
+		var recPath = newRecord.web_page_to_path.getRecord(newRecord.web_page_to_path.newRecord(false,true))
+		recPath.flag_default = 1
+		recPath.path = pathName
+		recPath.id_site = siteID
+		
+		return newRecord
+	}
+	else {
+		plugins.dialogs.showErrorDialog(
+						'Error',
+						'You must add a site record first'
+				)
+	}
+}
+
+/**
  *
  * @properties={typeid:24,uuid:"51A1F298-56F1-4D79-962F-795D222ED3CC"}
  */
