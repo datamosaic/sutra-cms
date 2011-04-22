745b5bc25a5eeacbf9aee4b29b1bdc2f8b702301
Kabootit <kabootit@users.noreply.github.com>
2011-04-22 20:13:23 +0000

    multi-site grouping

---
+++ _dsa_mosaic_WEB_cms/forms/WEB_0F_install__multisite.js
@@ -0,0 +1,10 @@
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @properties={typeid:24,uuid:"E25AE669-7ACF-4095-B2E5-6D986327BE38"}
+ */
+function ADD_sites(event) {
+	plugins.dialogs.showInfoDialog('Coming','Soon')
+}
