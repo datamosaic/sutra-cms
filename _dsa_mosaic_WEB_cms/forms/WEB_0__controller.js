e4283b8931fc0c91ca9f8a43408e6a80609517d4
Kabootit <kabootit@users.noreply.github.com>
2011-05-11 20:39:02 +0000

    update meta version info

--- _dsa_mosaic_WEB_cms/forms/WEB_0__controller.js
+++ _dsa_mosaic_WEB_cms/forms/WEB_0__controller.js
@@ -25,7 +25,7 @@ function CONTROLLER(app, session, request, response, mode)
 	
 	// initialize good dataset to return to jsp
 	var results = databaseManager.createEmptyDataSet(0,["key","value"])
-	results.addRow(["cmsVersion","Sutra CMS - 2.0 alpha 2"])
+	results.addRow(["cmsVersion","Sutra CMS - 2.0 alpha 3"])
 	
 	// STEP 1: Setup
 	var obj = CONTROLLER_setup(results, app, session, request, response, mode)
