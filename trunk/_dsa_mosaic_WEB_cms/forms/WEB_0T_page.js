5c47e5804abf06de6f155acd97c39c8fed585e03
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2011-10-25 09:24:01 +0000

    rename page's "update theme" to "refresh theme" to be consistent with theme and block actions

--- trunk/_dsa_mosaic_WEB_cms/forms/WEB_0T_page.js
+++ trunk/_dsa_mosaic_WEB_cms/forms/WEB_0T_page.js
@@ -68,7 +68,7 @@ function ACTIONS_list() {
 	//list of actions
 	var valueList = [
 			'Duplicate record',
-			'Update theme...',
+			'Refresh theme...',
 			'-',
 			'Toggle open',
 			'Toggle closed',
@@ -213,7 +213,7 @@ function ACTIONS_list_control(input,scopeType,scopeValue) {
 			REC_duplicate()
 			break
 	
-		case 1:
+		case 1: //refresh theme
 			var input = plugins.dialogs.showQuestionDialog(
 						'Update theme',
 						'Do you want to keep current data ore reset to the theme\'s defaults?',
