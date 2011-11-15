0a4e64e5b6ee1e509f9553e057aed9ae64821d8d
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2011-11-15 19:05:15 +0000

    page > gui mode > visit button tweaks

--- trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_page__design.js
+++ trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_page__design.js
@@ -240,10 +240,10 @@ function REC_on_select(event,skipLoad,verIndex,fireSelect,areaName,blockIndex) {
 			}
 			
 		 	//when called from event (not programatically) update tooltip on visit button
-			if (event) {
-			 	//set tooltip of visit with link
-				forms.WEB_0F_page__design_1F__button_tab.elements.btn_visit.toolTipText = forms.WEB_0F_page__design_1F__button_tab.VISIT_page(null,true)
-			}
+//			if (event) {
+//			 	//set tooltip of visit with link
+//				forms.WEB_0F_page__design_1F__button_tab.elements.btn_visit.toolTipText = forms.WEB_0F_page__design_1F__button_tab.VISIT_page(null,true)
+//			}
 			
 			//page type ui differences
 			PAGE_type_display(!pageValid ? 'Page invalid' : null)
