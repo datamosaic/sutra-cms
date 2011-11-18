5d5cfbc59848e5aa3e6aed36ac4a16d0df1baea7
Kabootit <kabootit@users.noreply.github.com>
2011-11-18 15:31:48 +0000

    when real mode form touched at login, spawn headless client and nothing else

--- _dsa_mosaic_WEB_cms/forms/WEB_0F_page__browser.js
+++ _dsa_mosaic_WEB_cms/forms/WEB_0F_page__browser.js
@@ -13,119 +13,132 @@ var _license_dsa_mosaic_WEB_cms = 'Module: _dsa_mosaic_WEB_cms \
  * @properties={typeid:24,uuid:"60C0F66F-9740-428F-8C64-BE5870650741"}
  */
 function REC_on_select(event,webMode,skipLoad,verIndex,fireSelect) {
-	//when in data sutra and notifications enabled (and they're not already on, do them)
-	if (application.__parent__.solutionPrefs && solutionPrefs.config && solutionPrefs.config.currentFormName) {
-		var formName = solutionPrefs.config.currentFormName
-		var serverName = forms[formName].controller.getServerName()
-		var tableName = forms[formName].controller.getTableName()
-		var currentNavItem = solutionPrefs.config.currentFormID
-		var rowSelected = forms[formName].foundset.getSelectedIndex()
+	//don't run on initial load of solution
+	if (application.getApplicationType() != APPLICATION_TYPES.HEADLESS_CLIENT && globals.WEB_block_on_select) {
 		
-		//show busy indicator while changing record
-		if (navigationPrefs.byNavItemID[currentNavItem].navigationItem.ulBusyIndicator) {
-			var busyIndicator = true
+		//when in data sutra and notifications enabled (and they're not already on, do them)
+		if (application.__parent__.solutionPrefs && solutionPrefs.config && solutionPrefs.config.currentFormName) {
+			var formName = solutionPrefs.config.currentFormName
+			var serverName = forms[formName].controller.getServerName()
+			var tableName = forms[formName].controller.getTableName()
+			var currentNavItem = solutionPrefs.config.currentFormID
+			var rowSelected = forms[formName].foundset.getSelectedIndex()
 			
-			globals.CODE_cursor_busy(true)
-			application.updateUI()
+			//show busy indicator while changing record
+			if (navigationPrefs.byNavItemID[currentNavItem].navigationItem.ulBusyIndicator) {
+				var busyIndicator = true
+				
+				globals.CODE_cursor_busy(true)
+				application.updateUI()
+			}
+			
+			//LOG record navigation
+			//only run when using query based way to hit repository
+			if (solutionPrefs.repository && solutionPrefs.repository.allFormsByTable && solutionPrefs.repository.allFormsByTable[serverName] && solutionPrefs.repository.allFormsByTable[serverName][tableName] && solutionPrefs.repository.allFormsByTable[serverName][tableName].primaryKey) {
+				var pkName = solutionPrefs.repository.allFormsByTable[serverName][tableName].primaryKey
+				var pkActedOn = forms[formName][pkName]
+			}
+			else {
+				var pkName = 'repositoryAPINotImplemented'
+				var pkActedOn = 0
+			}
+			
+			//record not clicked on before, throw up busy bar and busy cursor
+			var record = forms[formName].foundset.getRecord(rowSelected)
+			if (navigationPrefs.byNavItemID[currentNavItem].navigationItem.initialRecord && !navigationPrefs.byNavItemID[currentNavItem].listData.visitedPKs[record[pkName]]) {
+				var recNotLoaded = true
+				
+				//don't turn busy indicator on if it is already on
+				if (!busyIndicator) {
+					globals.CODE_cursor_busy(true)
+				}
+				globals.TRIGGER_progressbar_start(-273, navigationPrefs.byNavItemID[currentNavItem].navigationItem.initialRecordLabel)
+			}
+			
+			//save currently selected index
+			navigationPrefs.byNavItemID[currentNavItem].listData.index.selected = rowSelected
+			
+			//save time when pk of this record last accessed
+			navigationPrefs.byNavItemID[currentNavItem].listData.visitedPKs[(forms[formName][pkName] != 'repositoryAPINotImplemented') ? forms[formName][pkName] : 0] = application.getServerTimeStamp()
 		}
 		
-		//LOG record navigation
-		//only run when using query based way to hit repository
-		if (solutionPrefs.repository && solutionPrefs.repository.allFormsByTable && solutionPrefs.repository.allFormsByTable[serverName] && solutionPrefs.repository.allFormsByTable[serverName][tableName] && solutionPrefs.repository.allFormsByTable[serverName][tableName].primaryKey) {
-			var pkName = solutionPrefs.repository.allFormsByTable[serverName][tableName].primaryKey
-			var pkActedOn = forms[formName][pkName]
+		//this method is the same as...
+		var formName = 'WEB_0F_page__design'
+		
+		//when newly added page, skip this
+		if (!forms.WEB_0T_page._addRecord) {
+			//halt additional on select firing
+	//		forms.WEB_0F_page__design_1F_version_2L_scope._skipSelect = true
+			
+			//select page version of tri globals
+		 	forms.WEB_0F_page.SET_globals()
+			
+			//set up valuelists for tri globals
+			var pageValid = forms.WEB_0F_page.SET_valuelists()
+			
+			//set version junks
+			var activeInfo = forms[formName].SET_versions(skipLoad,!pageValid)
+			
+			//don't change anything if we're not loading in the versions
+			if (!skipLoad) {
+				//specified index to be selected
+				if (verIndex) {
+					//set selected index
+					forms.WEB_0F_page__design_1F_version.foundset.setSelectedIndex(verIndex)
+				}
+				//there is info about the active version
+				else if (activeInfo) {
+					//set selected index
+					forms.WEB_0F_page__design_1F_version.foundset.setSelectedIndex(activeInfo.position)
+					
+					//set version to be the active one
+					globals.WEB_page_version = activeInfo.record.id_version
+				}
+				//set to first thing in the list (most recent)
+				else {
+					globals.WEB_page_version = application.getValueListItems('WEB_page_version').getValue(1,2)
+				}
+			}
+			
+			//are edits allowed
+	//		var editAllow = forms.WEB_0F_page.ACTION_edit_get()
+			
+			//make sure edit button not showing if not supposed to be
+				//MEMO: probably don't want to force a save here...just need edit/save button reset to normal
+			forms.WEB_TB__web_mode.ACTION_save()
+			forms.WEB_TB__web_mode.TOGGLE_edit()
+			
+			//only show versions and groups if there is more than one defined
+			forms.WEB_TB__web_mode.TOGGLE_group()
+			forms.WEB_TB__web_mode.TOGGLE_version()
+			
+			//fill the browser bean with url to load
+			URL_update(webMode)
+			
+			//highlight edit mode
+			if (webMode) {
+				forms.WEB_TB__web_mode.ACTION_edit()
+			}
+		}
+		
+		//record was not in memory, turn off busy bar and busy cursor
+		if (recNotLoaded) {
+			globals.TRIGGER_progressbar_stop()
+			globals.CODE_cursor_busy(false)	
+		}
+		//changing record, finished turn off busy indicatar
+		else if (busyIndicator) {
+			globals.CODE_cursor_busy(false)	
+		}
+	}
+	//hit up headless client
+	else {
+		if (!elements.bn_browser) {
+			globals.WEB_browser_error()
 		}
 		else {
-			var pkName = 'repositoryAPINotImplemented'
-			var pkActedOn = 0
+			elements.bn_browser.navigateTo(globals.WEBc_markup_link_page(id_page))
 		}
-		
-		//record not clicked on before, throw up busy bar and busy cursor
-		var record = forms[formName].foundset.getRecord(rowSelected)
-		if (navigationPrefs.byNavItemID[currentNavItem].navigationItem.initialRecord && !navigationPrefs.byNavItemID[currentNavItem].listData.visitedPKs[record[pkName]]) {
-			var recNotLoaded = true
-			
-			//don't turn busy indicator on if it is already on
-			if (!busyIndicator) {
-				globals.CODE_cursor_busy(true)
-			}
-			globals.TRIGGER_progressbar_start(-273, navigationPrefs.byNavItemID[currentNavItem].navigationItem.initialRecordLabel)
-		}
-		
-		//save currently selected index
-		navigationPrefs.byNavItemID[currentNavItem].listData.index.selected = rowSelected
-		
-		//save time when pk of this record last accessed
-		navigationPrefs.byNavItemID[currentNavItem].listData.visitedPKs[(forms[formName][pkName] != 'repositoryAPINotImplemented') ? forms[formName][pkName] : 0] = application.getServerTimeStamp()
-	}
-	
-	//this method is the same as...
-	var formName = 'WEB_0F_page__design'
-	
-	//when newly added page, skip this
-	if (!forms.WEB_0T_page._addRecord) {
-		//halt additional on select firing
-//		forms.WEB_0F_page__design_1F_version_2L_scope._skipSelect = true
-		
-		//select page version of tri globals
-	 	forms.WEB_0F_page.SET_globals()
-		
-		//set up valuelists for tri globals
-		var pageValid = forms.WEB_0F_page.SET_valuelists()
-		
-		//set version junks
-		var activeInfo = forms[formName].SET_versions(skipLoad,!pageValid)
-		
-		//don't change anything if we're not loading in the versions
-		if (!skipLoad) {
-			//specified index to be selected
-			if (verIndex) {
-				//set selected index
-				forms.WEB_0F_page__design_1F_version.foundset.setSelectedIndex(verIndex)
-			}
-			//there is info about the active version
-			else if (activeInfo) {
-				//set selected index
-				forms.WEB_0F_page__design_1F_version.foundset.setSelectedIndex(activeInfo.position)
-				
-				//set version to be the active one
-				globals.WEB_page_version = activeInfo.record.id_version
-			}
-			//set to first thing in the list (most recent)
-			else {
-				globals.WEB_page_version = application.getValueListItems('WEB_page_version').getValue(1,2)
-			}
-		}
-		
-		//are edits allowed
-//		var editAllow = forms.WEB_0F_page.ACTION_edit_get()
-		
-		//make sure edit button not showing if not supposed to be
-			//MEMO: probably don't want to force a save here...just need edit/save button reset to normal
-		forms.WEB_TB__web_mode.ACTION_save()
-		forms.WEB_TB__web_mode.TOGGLE_edit()
-		
-		//only show versions and groups if there is more than one defined
-		forms.WEB_TB__web_mode.TOGGLE_group()
-		forms.WEB_TB__web_mode.TOGGLE_version()
-		
-		//fill the browser bean with url to load
-		URL_update(webMode)
-		
-		//highlight edit mode
-		if (webMode) {
-			forms.WEB_TB__web_mode.ACTION_edit()
-		}
-	}
-	
-	//record was not in memory, turn off busy bar and busy cursor
-	if (recNotLoaded) {
-		globals.TRIGGER_progressbar_stop()
-		globals.CODE_cursor_busy(false)	
-	}
-	//changing record, finished turn off busy indicatar
-	else if (busyIndicator) {
-		globals.CODE_cursor_busy(false)	
 	}
 }
 
