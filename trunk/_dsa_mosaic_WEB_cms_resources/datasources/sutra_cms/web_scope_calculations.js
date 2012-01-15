8f33dd792d7ece3d10b8aa6a1eb2d4120737447e
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2012-01-15 19:09:14 +0000

    block builder   - allow to add multiple instances per site   - add new column for storing configuration data for block types

--- trunk/_dsa_mosaic_WEB_cms_resources/datasources/sutra_cms/web_scope_calculations.js
+++ trunk/_dsa_mosaic_WEB_cms_resources/datasources/sutra_cms/web_scope_calculations.js
@@ -1,4 +1,46 @@
 /**
+ * @properties={type:12,typeid:36,uuid:"15E543A9-8DE6-4070-B6D5-DD6DE283909C"}
+ */
+function display_delete() {
+	var html = '<html><head></head><body><center>'
+	
+	//this is not a block builder component, can be deleted
+	if (true){//!parent_id_scope) {
+		html += '<img height="20" width="20" src="media:///btn_delete.png">'
+	}
+	//show nothing
+	else {
+		html += ''
+	}
+	
+	html += '</center></body></html>'
+	
+	return html
+}
+
+/**
+ * @properties={type:12,typeid:36,uuid:"F85FCCC2-D178-43FC-B736-973FB01D9BC7"}
+ */
+function display_flag_active() {
+	var html = '<html><head></head><body><center>'
+	
+	//this is not a block builder component, can be (de-)activated
+	if (true){//!parent_id_scope) {
+		//show correct state of checkbox
+		var img = (false) ? 'ballot_mac_unselect_blank' : 'ballot_mac_unselect_check'
+		html += '<img height="18" width="20" src="media:///' + img + '.png">'
+	}
+	//show nothing
+	else {
+		html += ''
+	}
+	
+	html += '</center></body></html>'
+	
+	return html
+}
+
+/**
  * Calculate the row background color.
  *
  * @param {Number} index row index
@@ -16,8 +58,12 @@ function row_background(index, selected, elementType, dataProviderID, edited) {
 	
  	//white/tan with medium blue highlighter and green if a scrapbook
 	if (selected) {
+		//block builder sub blocks
+		if (false) {//parent_id_scope) {
+			return '#EAEAEA'
+		}
 		//there are blocks and they aren't unique
-		if (scrapbook) {
+		else if (scrapbook) {
 			return '#B6E6B6'
 		}
 		else {
@@ -25,9 +71,15 @@ function row_background(index, selected, elementType, dataProviderID, edited) {
 		}
 	}
 	else {
-		if (scrapbook) {
+		//block builder sub blocks
+		if (false) {//parent_id_scope) {
+			return '#EFEFEF'
+		}
+		//scrapbook
+		else if (scrapbook) {
 			return '#DBF5DB'
 		}
+		//normal row alternation
 		else {
 			if (index % 2 == 0) {
 				return '#F7F8EF'
