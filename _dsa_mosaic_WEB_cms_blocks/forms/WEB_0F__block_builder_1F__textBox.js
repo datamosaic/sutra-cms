10a31bd48a5ef37a75bac7c52095e6fd4bfc2e50
Kabootit <kabootit@users.noreply.github.com>
2012-01-16 00:05:25 +0000

    block builder   - copy object data across when creating new block   - gui mode representation

---
+++ _dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__block_builder_1F__textBox.js
@@ -0,0 +1,36 @@
+/**
+ * @properties={typeid:35,uuid:"FEE96157-F927-477F-B800-0522497672E4"}
+ */
+var _license_dsa_mosaic_WEB_cms_blocks = 'Module: _dsa_mosaic_WEB_cms_blocks \
+									Copyright (C) 2011 Data Mosaic \
+									MIT Licensed';
+
+/**
+ * @properties={typeid:35,uuid:"27A71377-F68E-4445-82B5-DAB9503F3C07"}
+ */
+var _text = null;
+
+/**
+ * Handle changed data.
+ *
+ * @param {Object} oldValue old value
+ * @param {Object} newValue new value
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @returns {Boolean}
+ *
+ * @properties={typeid:24,uuid:"A620C9C7-36EF-43FD-83AC-A5EBBD2BD0C4"}
+ */
+function onDataChange(oldValue, newValue, event) {
+	forms.WEB_0F__block_builder._blockList[forms.WEB_0F__block_builder_1L__fields.row_order].data.data = newValue
+}
+
+/**
+ * @properties={typeid:24,uuid:"6FB5BFDF-6425-4BF1-8542-6B5EF59023A2"}
+ */
+function INIT_data(data) {
+	if (data) {
+		_text = data.data
+		elements.lbl_title.text = data.label
+	}
+}
---
+++ _dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__block_builder_1L__fields.js
@@ -0,0 +1,27 @@
+/**
+ * @properties={typeid:35,uuid:"886E521A-1826-4A9F-AA05-A622716F4FDB"}
+ */
+var _license_dsa_mosaic_WEB_cms_blocks = 'Module: _dsa_mosaic_WEB_cms \
+									Copyright (C) 2011 Data Mosaic \
+									MIT Licensed';
+
+/**
+ * Handle record selected.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @properties={typeid:24,uuid:"1A67C02F-D927-47CD-ABC1-41196D1871A9"}
+ */
+function REC_on_select(event) {
+	//grab data for selected record
+	var fieldData = forms.WEB_0F__block_builder._blockList[row_order]
+	
+	var formName = 'WEB_0F__block_builder' + '_1F__' + fieldData.data.type
+	
+	//load selected block type
+	forms.WEB_0F__block_builder.elements.tab_detail.removeAllTabs()
+	forms.WEB_0F__block_builder.elements.tab_detail.addTab(forms[formName])
+	
+	//punch data in
+	forms[formName].INIT_data(fieldData.data)
+}
