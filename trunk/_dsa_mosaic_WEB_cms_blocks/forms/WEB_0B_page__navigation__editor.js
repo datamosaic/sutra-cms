581ebc2f20ab08143b6f84850aab921ff03714fa
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2012-12-03 16:10:49 +0000

    Bring back to main cms repo  - servoy 61

--- trunk/_dsa_mosaic_WEB_cms_blocks/forms/WEB_0B_page__navigation__editor.js
+++ trunk/_dsa_mosaic_WEB_cms_blocks/forms/WEB_0B_page__navigation__editor.js
@@ -1,4 +1,6 @@
 /**
+ * @type {String}
+ *
  * @properties={typeid:35,uuid:"4FDACEFD-6F96-46F7-827B-375E25824AD6"}
  */
 var _license_dsa_mosaic_WEB_cms_blocks = 'Module: _dsa_mosaic_WEB_cms_blocks \
@@ -6,6 +8,8 @@ var _license_dsa_mosaic_WEB_cms_blocks = 'Module: _dsa_mosaic_WEB_cms_blocks \
 									MIT Licensed';
 
 /**
+ * @type {String}
+ *
  * @properties={typeid:35,uuid:"231F59B7-E8CF-417E-A58C-ED480562C4D5"}
  */
 var _htmlNavigation = null;
