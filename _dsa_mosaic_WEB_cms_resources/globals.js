53055fbf093856e5c0fe21a4238bb06654ba8e95
Kabootit <kabootit@users.noreply.github.com>
2012-12-21 20:51:36 +0000

    Added to CMS API: Tim (lite): A tiny, secure JavaScript micro-templating script.    github.com/premasagar/tim @example  		CMS.markup.merge("Hello {{place}}", {place: "world"})  			> Hello world

--- _dsa_mosaic_WEB_cms_resources/globals.js
+++ _dsa_mosaic_WEB_cms_resources/globals.js
@@ -69,7 +69,10 @@ var CMS = {
 					},
 				saveResponseData : function() {
 						return globals.WEBc_markup_block_saveResponse(globals.CMS.data)
-					}
+					},
+				merge : function (/**String*/ template, /**Object*/ data) {
+					return WEBc_markup_merge(template, data)
+				}
 			},
 		session : {
 				clearData : function(/**String*/ sessionID, /**String*/ dataKey) {
@@ -2643,4 +2646,51 @@ function WEBc_data_strip_punctuation(str) {
      
   return results
 
+}
+
+
+/**
+ * Tim (lite): A tiny, secure JavaScript micro-templating script.
+ *   github.com/premasagar/tim
+ *   
+ * @param {String} template
+ * @param {Object} data
+ * 
+ * @example 
+ * 		CMS.markup.merge("Hello {{place}}", {place: "world"})
+ * 			> Hello world
+ * 		CMS.markup.merge("Hello {{place}}. My name is {{person.name}}.", { place: "Brighton", person: { name: "Prem" } })
+ *			> Hello Brighton. My name is Prem.
+ *
+ * @properties={typeid:24,uuid:"E93C4B64-5AC4-4A50-A6EF-8506C5D07371"}
+ */
+function WEBc_markup_merge(template, data){
+
+    var start   = "{{",
+        end     = "}}",
+        path    = "[a-z0-9_][\\.a-z0-9_]*", // e.g. config.person.name
+        pattern = new RegExp(start + "\\s*("+ path +")\\s*" + end, "gi"),
+        undef; 
+
+        // Merge data into the template string
+        return template.replace(pattern, function(tag, token){
+            path = 	token.split(".")
+            var  	len = path.length,
+                	lookup = data,
+                	i = 0;
+
+            for (; i < len; i++){
+                lookup = lookup[path[i]];
+                
+                // Property not found
+                if (lookup === undef){
+                    throw "tim: '" + path[i] + "' not found in " + tag;
+                }
+                
+                // Return the required value
+                if (i === len - 1){
+                    return lookup;
+                }
+            }
+        });
 }
