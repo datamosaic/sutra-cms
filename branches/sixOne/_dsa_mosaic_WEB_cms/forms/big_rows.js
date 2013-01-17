3798d9c994b3199274d2354447c61eaa36d51b18
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2013-01-17 20:05:59 +0000

    close down 61 webclient branch

--- branches/sixOne/_dsa_mosaic_WEB_cms/forms/big_rows.js
+++
@@ -1,8 +0,0 @@
-/**
- * @type {String}
- *
- * @properties={typeid:35,uuid:"B3A93FDF-D120-4DF8-8CB4-42A81F6D5614"}
- */
-var _license_dsa_mosaic_WEB_cms = 'Module: _dsa_mosaic_WEB_cms \
-									Copyright (C) 2011, 2012 Data Mosaic \
-									MIT Licensed';
