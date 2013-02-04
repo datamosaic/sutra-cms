05f11f7c85edd96aa15a19c13db12ee7fe4e03ec
Kabootit <kabootit@users.noreply.github.com>
2013-02-04 16:58:52 +0000

    update version to 4.0b1

--- _dsa_mosaic_WEB_cms/forms/WEB_0__controller.js
+++ _dsa_mosaic_WEB_cms/forms/WEB_0__controller.js
@@ -26,7 +26,7 @@ function CONTROLLER(app, session, request, response, mode) {
 //	globals.CMS = eval(solutionModel.getGlobalVariable('CMS').defaultValue)
 	
 	// CMS Version
-	var cmsVersion = "3.0b3"
+	var cmsVersion = "4.0b1"
 
 	// initialize good dataset to return to jsp
 	var results = databaseManager.createEmptyDataSet(0,["key","value"])
