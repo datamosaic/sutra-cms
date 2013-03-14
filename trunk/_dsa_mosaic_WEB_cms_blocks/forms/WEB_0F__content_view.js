c6464b0f082bb961d5b8452d155703b7ac49baac
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2013-03-14 22:55:45 +0000

    beef up content editor and image insertion allow to import image from real mode real mode mini-saves don't follow normal edit-mode rules (they save) filter bar on image picker

--- trunk/_dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__content_view.js
+++ trunk/_dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__content_view.js
@@ -14,10 +14,53 @@ var _license_dsa_mosaic_WEB_cms_blocks = 'Module: _dsa_mosaic_WEB_cms_blocks \
  */
 function INIT_data() {
 	var data = globals.CMS.ui.getData(controller.getName())
+	var baseHREF = globals.WEBc_markup_link_base(forms.WEB_0F_page.id_page)
 	
-	var html = '<html><body><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>'
+	var html = '<html>'
+	var head = <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<base href={baseHREF} />
+		<script src="/resources/js/jquery-1.4.min.js" language="JavaScript" type="text/javascript">//</script>
+		<script type="text/javascript">
+		//<![CDATA[
+			$(function() {
+				//on load method that attaches callback to every index_edit link
+				$("a[href*='index_edit.jsp']").click(function(event){
+					
+					//stop default browser behaviour
+					event.preventDefault();
+				
+					//find pageID
+					var hashes = this.href.slice(this.href.indexOf('?') + 1).split('&');
+					for (var i = 0; i < hashes.length; i++) {
+						var hash = hashes[i].split('=');
+						if (hash[0] == 'id') {
+							var pageID = hash[1];
+							break
+						}
+					}
+					
+					//set page in Servoy, triggering refresh, etc
+					sendNSCommand("WEB_0T_page.SET_page",pageID);
+				});
+			});
+		//]]>
+		</script>
+	</head>
+	
+	var sanitize = head.toXMLString()
+	
+	//cdata linebreak issue
+	var regex = new RegExp('(//)(\\s*)(<!\\[CDATA\\[)')
+	var match = regex.exec(sanitize)
+	sanitize = utils.stringReplace(sanitize,match[0],match[1] + match[3] + '\n')
+	
+	html += sanitize + '<body>'
 	if (data.Content) {
-		html += data.Content
+		// replace out place holders (DS_* links)
+		var markup = globals.WEBc_markup_link_internal(data.Content,null,'Edit')
+		
+		html += markup
 	}
 	html += '</body></html>'
 	
