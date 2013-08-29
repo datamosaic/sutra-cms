4baaa2eb1db405b24c60a6bb6429b00d3941c13d
Kabootit <kabootit@users.noreply.github.com>
2013-08-29 09:20:17 +0000

    new flag for default block when no methods named VIEW/CONTROLLER_default

--- _dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__collection.js
+++ _dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__collection.js
@@ -41,6 +41,7 @@ function INIT_block() {
 	
 	// block views
 	block.views = globals.CMS.ui.getMethods(controller.getName(), "VIEW")
+	block.defaultView = 'VIEW_blog_detail'
 	
 	// block client actions - Block
 	block.clientActionsBlock = globals.CMS.ui.getMethods(controller.getName(), "BLOCK")
