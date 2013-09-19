a0c08dbeb9128451934c3303e99c76a8cd472951
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2013-09-19 09:59:21 +0000

    block builder can make layouts (https://trello.com/c/8TrF7CbY)

--- trunk/_dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__block_builder_view.js
+++ trunk/_dsa_mosaic_WEB_cms_blocks/forms/WEB_0F__block_builder_view.js
@@ -9,50 +9,64 @@ var _license_dsa_mosaic_WEB_cms_blocks = 'Module: _dsa_mosaic_WEB_cms_blocks \
 
 /**
  * Update display as needed when block selected.
- * 
+ *
  * @properties={typeid:24,uuid:"F617323D-990C-4AD0-B9A2-F4F29D82D220"}
  */
 function INIT_data() {
 	var formName = 'WEB_0F__block_builder'
-	
+
 	//grab all data for this block
 	var allFields = globals.CMS.ui.getData(controller.getName())
-	
+
 	var blockList = new Array()
-	
+
 	//build object for list to operate from
 	for (var i in allFields) {
 		var fieldData = JSON.parse(allFields[i])
-		
+
 		//we need order, type, and whatnot that may be buried in an array
 		var order = fieldData.order
 		var type = fieldData.type
-		
+
 		//create object to use for reference while on this block
 		blockList[order] = {
 						key : i,
 						type : type
 					}
-		
+
 		//this is a repeatable field that hasn't been used before
 		blockList[order].record = fieldData
 	}
-	
-	var html = '<html>'
-	html += globals.WEBb_index_edit() + '<body>'
-	for (var i = 1; i < blockList.length; i++) {
-		var blockItem = blockList[i]
-		if (blockItem) {
-			var method = 'MRKP_' + blockItem.type
-			
-			// this method exists
-			if (solutionModel.getForm(formName).getFormMethod(method)) {
-				html += globals.WEBc_markup_link_internal(forms[formName][method](blockItem.record),null,'Edit') + '\n'
+
+	var allStatic = blockList.every(function(item){return item.type == 'staticHTML'})
+
+	//if this is a layout block, use static
+	if (web_block_to_block_type.block_category == scopes.CMS._constant.blockCategory.LAYOUT) {// && allStatic) {
+		var html = '<html>'
+		html += globals.WEBb_index_edit() + '<body>'
+		html += '<h2>Layout block</h2><p>This block is a formatter for the following blocks.</p>'
+		html += '</body></html>'
+	}
+	else {
+		var html = '<html>'
+		html += globals.WEBb_index_edit() + '<body>'
+		for (var i = 1; i < blockList.length; i++) {
+			var blockItem = blockList[i]
+			if (blockItem) {
+				var method = 'MRKP_' + blockItem.type
+
+				// this method exists
+				if (solutionModel.getForm(formName).getFormMethod(method)) {
+					html += globals.WEBc_markup_link_internal(forms[formName][method](blockItem.record),null,'Edit') + '\n'
+				}
 			}
 		}
+		html += '</body></html>'
+
+		//replace out <<block>>
+		html = utils.stringReplace(html,'<<BLOCK>>','<em>&lt;&lt;BLOCK>><em>')
 	}
-	html += '</body></html>'
-	
+
 	if (elements.bn_browser) {
 		elements.bn_browser.html = html
 	}
