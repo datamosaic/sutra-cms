17811eed403d89ec3eec2d69f8e4dbdeaf4c7594
troy <troy@d5cd7dfc-54f3-48e5-84e6-f5021073d9c7>
2013-09-24 18:28:45 +0000

    deleting last page in site (https://trello.com/c/s1B30AWc) allow many page actions to be performed from UL

--- trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_page.js
+++ trunk/_dsa_mosaic_WEB_cms/forms/WEB_0F_page.js
@@ -26,11 +26,11 @@ var _lastToolbar = null;
  */
 function FORM_on_show(firstShow, event) {
 	//don't run in headless or web client unless sutra (they use whatever solution is activated as context)
-	if ((application.getApplicationType() == APPLICATION_TYPES.SMART_CLIENT || application.getApplicationType() == APPLICATION_TYPES.RUNTIME_CLIENT || 
+	if ((application.getApplicationType() == APPLICATION_TYPES.SMART_CLIENT || application.getApplicationType() == APPLICATION_TYPES.RUNTIME_CLIENT ||
 		(application.__parent__.solutionPrefs && solutionPrefs.config.webClient)) &&
 		//don't run if in a preload
 		!(application.__parent__.solutionPrefs && solutionPrefs.config.prefs.formPreloading)) {
-		
+
 		//save down currently selected toolbar
 		if (application.__parent__.solutionPrefs && !solutionPrefs.config.lockStatus) {
 			//when progressbar is active, take from previous toolbar
@@ -41,19 +41,19 @@ function FORM_on_show(firstShow, event) {
 			else {
 				_lastToolbar = forms[solutionPrefs.config.formNameBase + '__header__toolbar'].elements.tab_toolbar.tabIndex
 			}
-			
+
 			globals.WEBc_sutra_trigger('TRIGGER_toolbar_toggle',['Web Edit',true])
 			//make sure on page toolbar...disabled in wc for now
 			if (!solutionPrefs.config.webClient) {
 				globals.WEBc_sutra_trigger('TRIGGER_toolbar_set',['Web Edit'])
 			}
 		}
-		
+
 		//first time go to sitemap view (do at end so everything loaded already)
 		if (firstShow) {
 			globals.WEBc_sutra_trigger('TRIGGER_ul_tab_list',[scopes.CMS.util.getTreeForm(),'Sitemap',0])
 		}
-		
+
 		//in workflow maximized view or in webclient
 		if (solutionPrefs.config.webClient || firstShow && application.__parent__.solutionPrefs && (solutionPrefs.config.activeSpace == 'list' || solutionPrefs.config.activeSpace == 'workflow')) {
 			//switch modes
@@ -61,35 +61,35 @@ function FORM_on_show(firstShow, event) {
 			guiEvent.getElementName = function() {
 					return 'lbl_mode_live'
 				}
-			
+
 			forms.WEB_TB__web_mode.ACTION_mode(guiEvent)
 			return
 		}
-		
+
 		if (!utils.hasRecords(foundset)) {
 			//null out variables and valuelists
 			globals.WEB_page_version = null
 			globals.WEB_page_group = null
 			globals.WEB_page_language = null
 			globals.WEB_page_platform = null
-			
+
 			forms.WEB_0F_page__design.REC_on_select()
-			
+
 			//update tree
 			forms[scopes.CMS.util.getTreeForm()].TREE_refresh()
-			
+
 			//no records, dim out
 			globals.WEB_lock_workflow(true)
-			
+
 			//set elements appropriately
 			forms.WEB_TB__web_mode.controller.enabled = false
-			
+
 			forms.WEB_A__page.TOGGLE_edit_mode(false)
 		}
-		else {			
+		else {
 			//enable rec_on_select of the block type form
 			globals.WEB_block_on_select = true
-			
+
 			//need to reload the tree
 			if (forms[scopes.CMS.util.getTreeForm()]._refresh || firstShow) {
 				_loadFilters = true
@@ -109,30 +109,30 @@ function FORM_on_show(firstShow, event) {
  */
 function FORM_on_hide(event) {
 	//don't run in headless or web client (they use whatever solution is activated as context)
-	if ((application.getApplicationType() == APPLICATION_TYPES.SMART_CLIENT || application.getApplicationType() == APPLICATION_TYPES.RUNTIME_CLIENT || 
+	if ((application.getApplicationType() == APPLICATION_TYPES.SMART_CLIENT || application.getApplicationType() == APPLICATION_TYPES.RUNTIME_CLIENT ||
 		(application.__parent__.solutionPrefs && solutionPrefs.config.webClient)) &&
 		//don't run if in a preload
 		!(application.__parent__.solutionPrefs && solutionPrefs.config.prefs.formPreloading)) {
-		
+
 		//undo locked screen
 		globals.WEB_lock_workflow(false)
-		
+
 		forms.WEB_TB__web_mode.controller.enabled = true
-		
+
 		//restore last selected toolbar
 		if (application.__parent__.solutionPrefs && !solutionPrefs.config.lockStatus) {
 			//disable web edit toolbar
 			if (!solutionPrefs.config.webClient) {
 				globals.WEBc_sutra_trigger('TRIGGER_toolbar_toggle',['Web Edit',false])
 			}
-			
+
 			//make sure on whatever last toolbar was
 			solutionPrefs.config.lastSelectedToolbar = _lastToolbar
-			
+
 			_lastToolbar = null
 		}
 	}
-	
+
 	return true
 }
 
@@ -149,11 +149,11 @@ function FIND_restrict_site() {
 function FIND_post_process(count) {
 	//show correct list
 	var baseForm = solutionPrefs.config.formNameBase
-	
+
 	//which tab is this UL on
 	var ulTab = (navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].listData.withButtons) ? 2 : 3
 	var listTabForm = (solutionPrefs.config.webClient) ? forms.DATASUTRA_WEB_0F__list__universal : forms.DATASUTRA_0F_solution
-			
+
 	//only change tabs when we're not already in the right place
 	if (listTabForm.elements.tab_content_B.tabIndex != ulTab) {
 		//something found as a result of this find, show flat view
@@ -163,7 +163,7 @@ function FIND_post_process(count) {
 		//show tree view when currently on it
 		else {
 			listTabForm.elements.tab_content_B.tabIndex = navigationPrefs.byNavSetName.configPanes.itemsByName['Custom tab ' + solutionPrefs.config.currentFormID + ': WEB_0T_page'].listData.tabNumber
-			
+
 			//force to select correct record
 			forms[scopes.CMS.util.getTreeForm()].elements.bean_tree.selectionPath = forms[scopes.CMS.util.getTreeForm()].FIND_path(foundset.getSelectedRecord())
 		}
@@ -186,7 +186,7 @@ function ACTION_edit_get() {
 	else {
 		var editAllow = forms.WEB_A__page._editMode
 	}
-	
+
 	return editAllow
 }
 
@@ -198,14 +198,14 @@ function SET_globals() {
 	if (_loadFilters && utils.hasRecords(forms.WEB_0F_site.foundset)) {
 		//turn off default load
 		_loadFilters = false
-		
+
 		if (utils.hasRecords(foundset)) {
 			var siteRec = web_page_to_site.getSelectedRecord()
 		}
 		else {
 			var siteRec = forms.WEB_0F_site.foundset.getSelectedRecord()
 		}
-		
+
 		for (var i = 1; i <= siteRec.web_site_to_site_platform.getSize(); i++) {
 			var record = siteRec.web_site_to_site_platform.getRecord(i)
 			if (record.flag_default) {
@@ -213,7 +213,7 @@ function SET_globals() {
 				break
 			}
 		}
-		
+
 		for (var i = 1; i <= siteRec.web_site_to_site_language.getSize(); i++) {
 			var record = siteRec.web_site_to_site_language.getRecord(i)
 			if (record.flag_default) {
@@ -221,7 +221,7 @@ function SET_globals() {
 				break
 			}
 		}
-		
+
 		for (var i = 1; i <= siteRec.web_site_to_site_group.getSize(); i++) {
 			var record = siteRec.web_site_to_site_group.getRecord(i)
 			if (record.flag_default) {
@@ -229,14 +229,14 @@ function SET_globals() {
 				break
 			}
 		}
-		
-		//set globals 
+
+		//set globals
 		globals.WEB_page_platform = platform.id_site_platform.toString()
 		globals.WEB_page_language = language.id_site_language.toString()
 		globals.WEB_page_group = group.id_site_group.toString()
 	}
-	
-	
+
+
 	//store page version of platform
 	var pageVersion = null
 	if (utils.hasRecords(web_page_to_platform)) {
@@ -254,7 +254,7 @@ function SET_globals() {
 	else {
 		forms.WEB_0F_page__design_1F__header_display_2F_platform._platform.clear()
 	}
-	
+
 	//store page version of language
 	pageVersion = null
 	if (utils.hasRecords(web_page_to_language)) {
@@ -272,7 +272,7 @@ function SET_globals() {
 	else {
 		forms.WEB_0F_page__design_1F__header_display_2F_language._language.clear()
 	}
-	
+
 	//store page version of group
 	pageVersion = null
 	if (utils.hasRecords(web_page_to_group)) {
@@ -290,7 +290,7 @@ function SET_globals() {
 			forms.WEB_0F_page__design_1F__header_display_2F_group._group.clear()
 		}
 	}
-	
+
 	//update tooltips for page version globals
 	forms.WEB_0F_page__design_1F__header_display_2F_platform.SET_tooltip()
 	forms.WEB_0F_page__design_1F__header_display_2F_language.SET_tooltip()
@@ -306,61 +306,61 @@ function SET_valuelists() {
 	//set up valuelists showing all values for site, but with enabled versions for what selected
 		//return value is site dictionary pk
 	//TODO: big todo....all these finds for every combination in the versions table has to be slow
-	
+
 	var fsPlatformAll = forms.WEB_0F_site.web_site_to_site_platform
 	var fsLanguageAll = forms.WEB_0F_site.web_site_to_site_language
 	var fsGroupAll = forms.WEB_0F_site.web_site_to_site_group
-	
+
 	var fsPlatform = web_page_to_platform
 	var fsLanguage = web_page_to_language
 	var fsGroup = web_page_to_group
-	
+
 	var fsVersions = databaseManager.getFoundSet('sutra_cms','web_version')
-	
+
 	var valid
-	
+
 	var vlPlatformDisplay = new Array()
 	var vlPlatformReal = new Array()
 	var vlLanguageDisplay = new Array()
 	var vlLanguageReal = new Array()
 	var vlGroupDisplay = new Array()
 	var vlGroupReal = new Array()
-	
+
 //PLATFORMS
-	
+
 	//grab all created platforms for this page (used to check and see if page version created)
 	var keysPlatform = databaseManager.getFoundSetDataProviderAsArray(fsPlatform, 'id_site_platform')
 	keysPlatform = keysPlatform.map(function(item) {return item.toString()})
-	
+
 	//loop over all platforms and only show valid ones as an option
 	if (utils.hasRecords(fsPlatformAll)) {
 		for (var i = 1; i <= fsPlatformAll.getSize(); i++) {
 			//reset the check for validity to not valid
 			valid = false
-			
+
 			//site level record
 			var recSite = fsPlatformAll.getRecord(i)
-			
+
 			//1- is there a page scoped record
 			var posn = keysPlatform.indexOf(recSite.id_site_platform.toString())
 			if (posn >= 0) {
 				//page level record
 				var recPage = fsPlatform.getRecord(posn + 1)
-				
+
 				//2- is there a version stack
 				fsVersions.find()
 				fsVersions.id_platform = recPage.id_platform.toString()
 				var results = fsVersions.search()
 				if (results) {
 					valid = true
-					
+
 					//this is the selected record and it's valid
 					if (utils.hasRecords(forms.WEB_0F_page__design_1F__header_display_2F_platform._platform) && forms.WEB_0F_page__design_1F__header_display_2F_platform._platform.id_platform.toString() == recPage.id_platform.toString()) {
 						var platformValid = true
 					}
 				}
 			}
-			
+
 			//push on as valid
 			if (valid) {
 				var display = '<html><body><font color="#333333"><strong>' + recSite.platform_name + '</strong></font>'
@@ -374,30 +374,30 @@ function SET_valuelists() {
 			vlPlatformReal.push(recSite.id_site_platform)
 		}
 	}
-	
+
 	if (application.__parent__.solutionPrefs && solutionPrefs.config.webClient) {
 		vlPlatformDisplay = vlPlatformDisplay.map(function(item){return item.replace(/(<([^>]+)>)/ig,'')})
 	}
-	
+
 	//set valuelist
 	application.setValueListItems('WEB_page_platform',vlPlatformDisplay,vlPlatformReal)
-	
-	
+
+
 //LANGUAGES
-	
+
 	//grab all created languages for this page (used to check and see if page version created)
 	var keysLanguage = databaseManager.getFoundSetDataProviderAsArray(fsLanguage, 'id_site_language')
 	keysLanguage = keysLanguage.map(function(item) {return item.toString()})
-	
+
 	//loop over all languages and only show valid ones as an option
 	if (utils.hasRecords(fsLanguageAll)) {
 		for (var i = 1; i <= fsLanguageAll.getSize(); i++) {
 			//reset the check for validity to not valid
 			valid = false
-			
+
 			//site level record
 			var recSite = fsLanguageAll.getRecord(i)
-			
+
 			//selected platform valid, keep checking
 			if (platformValid) {
 				//1- is there a page scoped record
@@ -405,7 +405,7 @@ function SET_valuelists() {
 				if (posn >= 0) {
 					//page level record
 					var recPage = fsLanguage.getRecord(posn + 1)
-					
+
 					//2- is there a version stack
 					fsVersions.find()
 					fsVersions.id_platform = forms.WEB_0F_page__design_1F__header_display_2F_platform._platform.id_platform.toString()
@@ -413,7 +413,7 @@ function SET_valuelists() {
 					var results = fsVersions.search()
 					if (results) {
 						valid = true
-						
+
 						//this is the selected record and it's valid
 						if (utils.hasRecords(forms.WEB_0F_page__design_1F__header_display_2F_language._language) && forms.WEB_0F_page__design_1F__header_display_2F_language._language.id_language.toString() == recPage.id_language.toString()) {
 							var languageValid = true
@@ -421,7 +421,7 @@ function SET_valuelists() {
 					}
 				}
 			}
-			
+
 			//push on as valid
 			if (valid) {
 				var display = '<html><body><font color="#333333"><strong>' + recSite.language_name + '</strong></font>'
@@ -435,39 +435,39 @@ function SET_valuelists() {
 			vlLanguageReal.push(recSite.id_site_language)
 		}
 	}
-	
+
 	if (application.__parent__.solutionPrefs && solutionPrefs.config.webClient) {
 		vlLanguageDisplay = vlLanguageDisplay.map(function(item){return item.replace(/(<([^>]+)>)/ig,'')})
 	}
-	
+
 	//set valuelist
 	application.setValueListItems('WEB_page_language',vlLanguageDisplay,vlLanguageReal)
-	
-	
+
+
 //GROUPS
-	
+
 	//grab all created groups for this page (used to check and see if page version created)
 	var keysGroup = databaseManager.getFoundSetDataProviderAsArray(fsGroup, 'id_site_group')
 	keysGroup = keysGroup.map(function(item) {return item.toString()})
-	
+
 	//loop over all groups and only show valid ones as an option
 	if (utils.hasRecords(fsGroupAll)) {
 		for (var i = 1; i <= fsGroupAll.getSize(); i++) {
 			//reset the check for validity to not valid
 			valid = false
-			
+
 			//site level record
 			var recSite = fsGroupAll.getRecord(i)
-			
+
 			//selected platform and language valid, keep checking
 			if (platformValid && languageValid) {
-				
+
 				//1- is there a page scoped record
 				var posn = keysGroup.indexOf(recSite.id_site_group.toString())
 				if (posn >= 0) {
 					//page level record
 					var recPage = fsGroup.getRecord(posn + 1)
-					
+
 					//2- is there a version stack
 					fsVersions.find()
 					fsVersions.id_platform = forms.WEB_0F_page__design_1F__header_display_2F_platform._platform.id_platform.toString()
@@ -476,7 +476,7 @@ function SET_valuelists() {
 					var results = fsVersions.search()
 					if (results) {
 						valid = true
-						
+
 						//this is the selected record and it's valid
 						if (utils.hasRecords(forms.WEB_0F_page__design_1F__header_display_2F_group._group) && forms.WEB_0F_page__design_1F__header_display_2F_group._group.id_group.toString() == recPage.id_group.toString()) {
 							var groupValid = true
@@ -484,7 +484,7 @@ function SET_valuelists() {
 					}
 				}
 			}
-			
+
 			//push on as valid
 			if (valid) {
 				var display = '<html><body><font color="#333333"><strong>' + recSite.group_name + '</strong></font>'
@@ -498,15 +498,57 @@ function SET_valuelists() {
 			vlGroupReal.push(recSite.id_site_group)
 		}
 	}
-	
+
 	if (application.__parent__.solutionPrefs && solutionPrefs.config.webClient) {
 		vlGroupDisplay = vlGroupDisplay.map(function(item){return item.replace(/(<([^>]+)>)/ig,'')})
 	}
-	
+
 	//set valuelist
 	application.setValueListItems('WEB_page_group',vlGroupDisplay,vlGroupReal)
-	
-	
+
+
 	//the platform-language-group combo selected is valid
 	return platformValid && languageValid && groupValid
+}
+
+/**
+ * @properties={typeid:24,uuid:"5CBE21E1-0DCF-440C-B4BB-FCB82E6664E4"}
+ */
+function REC_new() {
+	forms[scopes.CMS.util.getTreeForm()].REC_new()
+}
+
+/**
+ * @properties={typeid:24,uuid:"1EEE918B-D834-46CF-8BA3-0267224C85EC"}
+ */
+function REC_delete() {
+	forms[scopes.CMS.util.getTreeForm()].REC_delete()
+}
+
+/**
+ * @properties={typeid:24,uuid:"16B9BA05-1D4E-41B3-832D-F9623F5DC869"}
+ */
+function REC_duplicate() {
+	forms[scopes.CMS.util.getTreeForm()].REC_duplicate()
+}
+
+/**
+ * @properties={typeid:24,uuid:"9293DE45-2E09-408A-9153-F3119869ED76"}
+ */
+function THEME_refresh() {
+	forms[scopes.CMS.util.getTreeForm()].ACTIONS_list_control(1)
+}
+
+/**
+ * @properties={typeid:24,uuid:"1DE92D15-7BDB-4522-9BD6-19FE698AF704"}
+ */
+function CACHE_flush() {
+	forms[scopes.CMS.util.getTreeForm()].ACTIONS_list_control(10)
+}
+
+/**
+ * @properties={typeid:24,uuid:"05BA1D05-244E-40FC-89AC-794210245966"}
+ */
+function CACHE_page() {
+	forms[scopes.CMS.util.getTreeForm()].ACTIONS_list_control(11)
 }
