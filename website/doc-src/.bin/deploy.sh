5a80722fbfe201665eb63ddccd6fb1a00f7a6930
Kabootit <kabootit@users.noreply.github.com>
2017-03-13 16:44:08 -0400

    deploy test

---
+++ website/doc-src/.bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# documentation
+rm -rf ../../docs/documentation
+cp -rf ../documentation ../../docs/documentation
+
+
+# website
+cp -rf ../favicon.ico ../../docs/favicon.ico
+cp -rf ../index.html ../../docs/index.html
+
+# git
+# git add -A
+# git commit -m 'update documentation'
+# git push 
